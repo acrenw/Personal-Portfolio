ef31d9485ebb758d6cf6c49e20d2212e1ed153d6
acrenw <wangc4574@wrdsb.ca>
2023-10-08 14:30:57 -0400

    made player have idle animation and position is updatable (class Player)

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
+// canvas setup
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = 1450
 canvas.height = 600
 
-// declaring the avatar image
-const avatar = new Image()
-avatar.src='images/avatar-idle-down.png'
+// declaring avatar images
+const avatarIdleUp = new Image()
+avatarIdleUp.src='images/avatar-idle-up.png'
 
-let x = 0
-let y = 0
+const avatarIdleDown = new Image()
+avatarIdleDown.src='images/avatar-idle-down.png'
+
+const avatarWalk1 = new Image()
+avatarIdleUp.src='images/avatar-idle-up.png' //change source later
+
+const avatarWalk2 = new Image()
+avatarIdleUp.src='images/avatar-idle-up.png' //change source later
+
+
+// classes
+class Player {
+    constructor (up, down, walk1, walk2, position) {
+        this.up = up
+        this.down = down
+        this.walk1 = walk1
+        this.walk2 = walk2
+        this.position = position
+    }
+
+    draw() {
+        if (frame%40 < 20) {
+            if (this.up.complete){
+                c.drawImage(this.up, this.position.x, this.position.y)
+            }
+        }
+        else {
+            if (this.down.complete){
+                c.drawImage(this.down, this.position.x, this.position.y)
+            }
+        }
+        
+    }
+
+    update() {
+        this.draw()
+        this.position.y++
+    }
+}
+
+
+const cera = new Player(avatarIdleUp, avatarIdleDown, avatarWalk1, avatarWalk2, {
+    x:0,
+    y:0,
+})
+
+
+let frame = 0
 
 // animation
 function animate() {
-    window.requestAnimationFrame(animate) //which function should i call over and over again
-
-    // drawing avatar
-    avatar.onload = function () {
-        c.drawImage(avatar, x, y)
-    }
-
-    y++
+    c.clearRect(0,0,canvas.width,canvas.height)
     
+    cera.update()
+
+    frame++
+    window.requestAnimationFrame(animate) //shcedules to run animate() on the next frame
 }
 
 animate()
