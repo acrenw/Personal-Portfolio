a03afdd7cd76b58a2a9093c94b8430b2de8d582f
acrenw <wangc4574@wrdsb.ca>
2023-10-08 21:09:24 -0400

    avatar turns around and doesnt go off screen now

--- script.js
+++ script.js
@@ -7,28 +7,31 @@ canvas.height = 600
 
 //variables
 const gravity = 1
+var playerLeft = false
+//cera stand = true
 
-// declaring avatar images
+// declaring images
 const avatarIdleUp = new Image()
 avatarIdleUp.src='images/avatar-idle-up.png'
 
 const avatarIdleDown = new Image()
 avatarIdleDown.src='images/avatar-idle-down.png'
 
-const avatarWalk1 = new Image()
-avatarIdleUp.src='images/avatar-idle-up.png' //change source later
+const introduce = new Image() 
+introduce.src='images/introduce.png'
 
-const avatarWalk2 = new Image()
-avatarIdleUp.src='images/avatar-idle-up.png' //change source later
+const learnMore = new Image() 
+learnMore.src='images/learnMore.png'
+
+const comment = new Image()
+comment.src='images/comment'
 
 
 // classes
 class Player {
-    constructor (up, down, walk1, walk2, position) {
+    constructor (up, down, position) {
         this.up = up
         this.down = down
-        this.walk1 = walk1
-        this.walk2 = walk2
         this.position = position
         this.velocity = {
             x: 0,
@@ -41,12 +44,28 @@ class Player {
     draw() {
         if (frame%40 < 20) {
             if (this.up.complete){
-                c.drawImage(this.up, this.position.x, this.position.y)
+                if (playerLeft){
+                    c.save(); // Save the current state
+                    c.scale(-1, 1); // Flip along y-axis
+                    c.drawImage(this.up, -this.position.x-this.width, this.position.y);
+                    c.restore(); // Restore to the previous state
+                }
+                else {
+                    c.drawImage(this.up, this.position.x, this.position.y)
+                }
             }
         }
         else {
             if (this.down.complete){
-                c.drawImage(this.down, this.position.x, this.position.y)
+                if (playerLeft){
+                    c.save(); // Save the current state
+                    c.scale(-1, 1); // Flip along y-axis
+                    c.drawImage(this.down, -this.position.x-this.width, this.position.y);
+                    c.restore(); // Restore to the previous state
+                }
+                else {
+                    c.drawImage(this.down, this.position.x, this.position.y)
+                }
             }
         }
         
@@ -58,19 +77,35 @@ class Player {
         if (this.width + this.position.x + this.velocity.x < canvas.width) {
             this.position.x += this.velocity.x
         }
-        
+        if (this.position.x + this.velocity.x <= 0) {
+            this.position.x -= this.velocity.x
+        }
+        if (this.position.y + this.velocity.y <= 0) {
+            this.position.y -= this.velocity.y
+        }
         if (this.height + this.position.y + this.velocity.y < canvas.height) {
             this.position.y += this.velocity.y
             this.velocity.y += gravity //add acceleration
         }
-        
     }
+
+    // introduce() { //draw speech bubble beside ceras head position
+    // }
+
+    // learnMore() {
+    // }
+
+    // throwElements() { //explode element icons from ceras coords
+    // }
+
+    // comment() {
+    // }
 }
 
 
-const cera = new Player(avatarIdleUp, avatarIdleDown, avatarWalk1, avatarWalk2, {
-    x:0,
-    y:0,
+const cera = new Player(avatarIdleUp, avatarIdleDown, {
+    x:canvas.width/2-100,
+    y:canvas.height-100,
 })
 
 
@@ -87,14 +122,54 @@ const keys = {
 // animation
 function animate() {
     c.clearRect(0,0,canvas.width,canvas.height)
+
+    //draw background (groud? floors?)
     
     //drawing updated player
     cera.update()
 
     //updating player's x positions
     cera.velocity.x = 0
-    if (keys.ArrowRight.pressed) cera.velocity.x = 5
-    else if (keys.ArrowLeft.pressed) cera.velocity.x = -5
+    if (keys.ArrowRight.pressed) {
+        cera.velocity.x = 5
+        playerLeft = false
+    }
+    else if (keys.ArrowLeft.pressed) {
+        cera.velocity.x = -5
+        playerLeft = true
+    }
+
+    // if mouse clicked on cera 
+    //      if cera stand
+    //          cera.introduce()
+    //          cera introduce = true
+    //          cera stand = false
+    //      else if cera introduce
+    //          cera.learnMore()
+    //          cera learn more = true
+    //          cera introduce = false
+    //      else if cera learn more
+    //          cera.throwElements()
+    //          cera throw elements = true
+    //          cera learn more = false
+    //      else if cera throw elements
+    //          cera.comment()
+
+
+    // if elements are thrown (about me, education, experience, projects, achievements, contact me)
+    //      //update element position based on whether cera touches them or not
+    //      //(physics- kick them away, jump on them, elements cant overlap with each other)
+
+    //      if mouse click on a coords
+    //          a-popup = true
+    //          set X (close window button) coord to wherever a's X coords are
+    //      //repeat for a-z
+
+    //      if a||b||...||z is true
+    //          cant click on any other elements
+    //          if mouse clicks on X coords
+    //              clear all popup windows
+    //              set all elements popups to false
 
     frame++
     window.requestAnimationFrame(animate) //shcedules to run animate() on the next frame
